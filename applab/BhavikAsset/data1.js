4e7cf46769beeea902eb77038c9dcec10581add1
bhavikdodda <bhavikdodda@gmail.com>
2025-01-18 11:32:41 +0530

    added k maps 2

--- applab/BhavikAsset/data1.js
+++ applab/BhavikAsset/data1.js
@@ -1,4 +1,4 @@
 window.data1=[["share Sudoku solver","https://studio.code.org/projects/applab/tq6f6xmxIqovL_yWTDpV0moGNpj5gC1yBz3GI2TfuII/edit","1/4/2025"],
-["Binary Clock - MathEnthusiast314","https://studio.code.org/projects/applab/yk3e7kmByzzbixyx4zsC8_AnXJpSmnm8uozUwCpyKCU/edit","4/7/2023"]
-
+["Binary Clock - MathEnthusiast314","https://studio.code.org/projects/applab/yk3e7kmByzzbixyx4zsC8_AnXJpSmnm8uozUwCpyKCU/edit","4/7/2023"],
+["K Maps 2","https://studio.code.org/projects/applab/TlsrPjMWTiTWVvhnjx4R8fdNgk3n4Ht5T_vkUhxHLWU/edit","12/22/2022"]
 ]
